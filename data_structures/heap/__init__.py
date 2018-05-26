d48c83ee2712afe85f689fa57c8d6c9acba92ee6
Oliver Brook <olibrook@gmail.com>
2018-05-26 14:46:19 -0700

    Working minheap

--- data_structures/heap/__init__.py
+++ data_structures/heap/__init__.py
@@ -1,35 +1,30 @@
+import collections
+
+
+Pair = collections.namedtuple('Pair', ['idx', 'v'])
+
 
 class MinHeap:
 
     def __init__(self):
         self.l = []
 
-    def get_left_child_idx(self, parent_idx):
-        return (2 * parent_idx) + 1
+    def left_child(self, idx):
+        c_idx = (2 * idx) + 1
+        return Pair(c_idx, self.l[c_idx]) if c_idx < len(self.l) else None
 
-    def get_right_child_idx(self, parent_idx):
-        return (2 * parent_idx) + 2
+    def right_child(self, idx):
+        c_idx = (2 * idx) + 2
+        return Pair(c_idx, self.l[c_idx]) if c_idx < len(self.l) else None
 
-    def get_parent_idx(self, child_idx):
-        return int((child_idx - 1) / 2)
+    def parent(self, idx):
+        p_idx = int((idx - 1) / 2)
+        return Pair(p_idx, self.l[p_idx]) if p_idx >= 0 else None
 
-    def has_parent(self, child_idx):
-        return self.get_parent_idx(child_idx) >= 0
-
-    def has_left_child(self, parent_idx):
-        return self.get_left_child_idx(parent_idx) < len(self.l)
-
-    def has_right_child(self, parent_idx):
-        return self.get_right_child_idx(parent_idx) < len(self.l)
-
-    def left_child(self, parent_idx):
-        return self.l[self.get_left_child_idx(parent_idx)]
-
-    def right_child(self, parent_idx):
-        return self.l[self.get_right_child_idx(parent_idx)]
-
-    def parent(self, child_idx):
-        return self.l[self.get_parent_idx(child_idx)]
+    def children(self, idx):
+        ret = (self.left_child(idx), self.right_child(idx))
+        ret = (x for x in ret if x is not None)
+        return list(ret)
 
     def swap(self, i1, i2):
         tmp = self.l[i1]
@@ -37,11 +32,64 @@ class MinHeap:
         self.l[i2] = tmp
 
     def peek(self):
-        # Find-min
         return self.l[0] if len(self.l) else None
 
-    def insert(self):
-        pass
-
     def pop(self):
-        pass
+        item = self.l.pop(0) if len(self.l) else None
+        if len(self.l):
+            self.l.insert(0, self.l.pop())
+            self.heapify_down()
+        return item
+
+    def add(self, v):
+        self.l.append(v)
+        self.heapify_up()
+
+    def heapify_up(self):
+        # Take the last element of the heap and lift it up until we reach a
+        # parent with a value less then the current one.
+        if len(self.l):
+            current_idx = len(self.l) - 1
+            while True:
+                parent = self.parent(current_idx)
+                if parent is None or self.l[current_idx] >= parent.v:
+                    break
+                else:
+                    self.swap(current_idx, parent.idx)
+                    current_idx = parent.idx
+
+    def heapify_down(self):
+        # Compare the root element to its children and swap root with the smallest
+        # of children. Do the same for next children after swap.
+        if len(self.l):
+            current_idx = 0
+            while True:
+                children = self.children(current_idx)
+                smallest = (
+                    min(children, key=lambda pair: pair.v) if children
+                    else None)
+                if smallest is None or self.l[current_idx] < smallest.v:
+                    break
+                self.swap(current_idx, smallest.idx)
+                current_idx = smallest.idx
+
+
+if __name__ == '__main__':
+    h = MinHeap()
+    h.add(1)
+    assert h.pop() == 1
+    h.add(3)
+    h.add(1)
+    h.add(1)
+    h.add(1)
+    h.add(1)
+    h.add(2)
+    assert h.pop() == 1
+    assert h.pop() == 1
+    assert h.pop() == 1
+    assert h.pop() == 1
+    assert h.pop() == 2
+    assert h.pop() == 3
+    assert h.pop() is None
+
+    print('Okay')
