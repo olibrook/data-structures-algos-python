b6da8acd698a98fa851d0f68c5db7bc8f65ca4b2
Oliver Brook <olibrook@gmail.com>
2018-06-30 14:37:45 -0700

    Tidy

--- pyalgos/puzzles/largest_rectangle_in_a_histogram.py
+++ pyalgos/puzzles/largest_rectangle_in_a_histogram.py
@@ -1,13 +1,14 @@
 """Largest rectangle in a histogram.
 
-Key to this problem is using a stack to keep track of the potential top-left
+Key to this problem is using a stack to keep track of top-left
 corners that could be used when shading a rectangle.
 
 Every time we see a > value than the last, it adds a new potential top-left
-corner that we need to keep track of.
+corner that we need to keep track of. To keep track of them, put values on a stack
+maintaining the invariant that higher stack entries have higher values.
 
-Every time we see a <= value than the last, we can discard candidates on the
-stack, reducing the number of areas we need to compute for each subsequent value.
+Every time the invariant would be violated we can discard stack entries, reducing the
+number of areas we need to compute for each subsequent value.
 
 Note that in the following sequence:
 
@@ -17,50 +18,28 @@ The presence of the 0 in the last position makes all previous values no longer
 candidates for the top-left corner.
 
 """
-import itertools
 import collections
 
 
 Point = collections.namedtuple('Point', ['x', 'y'])
 
 
-def coroutine():
-    """Coroutine implementation allows computation eg. from an iterator of values.
-
-    Call next() and then send() with each new val to get the current max-rect.
-
-    """
+def find(data):
     largest = 0
     stack = []
-    x = 0
-    while True:
-        y = yield
+    current = None
+    for x, y in enumerate(data):
         current = Point(x, y)
         popped = None
         while stack and current.y <= stack[-1].y:
             popped = stack.pop()
+            largest = max(largest, popped.y * (current.x - popped.x))
 
-        if popped:
-            stack.append(Point(popped.x, current.y))
+        l = popped.x if popped else current.x
+        stack.append(Point(l, current.y))
 
-        if not stack or stack[-1].y < current.y:
-            stack.append(current)
+    while stack and current:
+        popped = stack.pop()
+        largest = max(largest, popped.y * (current.x+1 - popped.x))
 
-        areas = (
-            min(left.y, current.y) * (current.x+1 - left.x)
-            for left in stack
-        )
-        areas = itertools.chain(areas, [largest])
-        largest = max(areas)
-        yield largest
-        x += 1
-
-
-def find(data):
-    """Find-max for a list of values"""
-    largest = 0
-    coro = coroutine()
-    for y in data:
-        next(coro)
-        largest = coro.send(y)
     return largest
